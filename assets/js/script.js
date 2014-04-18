1e0283b18510234027dad9d72e33e546b4692e6a
Benjamin Yu <benjamin.sichit.yu@gmail.com>
2014-04-18 15:04:17 -0400

    matrix animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,6 +23,8 @@ $(document).ready(function(){
         $('#main-sidebar').css('background', 'url('+mainImageSource+')');
         mainImage.remove();
     }
+
+    matrixAnimation();
 	
 });
 
@@ -32,3 +34,27 @@ $(window).load(function(){
 	$('.spinner').hide();
 
 });
+
+function matrixAnimation() {
+    var width = $('#matrix')[0].width;
+    var height = $('#matrix')[0].height;
+    var letters = [];
+    for (var i = 0, l = 20; i < l; i++) {
+        letters.push(Math.round(Math.random() * height/5))
+    }
+    var context = $('#matrix')[0].getContext('2d');
+    context.font="10px Play";
+
+    var draw = function () {
+        context.fillStyle = "rgba(0, 0, 0, 0.05)";
+        context.fillRect(0, 0, width, height);
+        context.fillStyle='#0F0';
+        letters.map(function(y_pos, index){
+            text = String.fromCharCode(3e4+Math.random()*33);
+            x_pos = index * 20;
+            context.fillText(text, x_pos, y_pos, 10);
+            letters[index] = (y_pos > height + Math.random() * 1e4) ? 0 : y_pos + 2;
+        });
+    };
+    setInterval(draw, 33);
+}
